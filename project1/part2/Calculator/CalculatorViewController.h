50247773f582aaf98e3821d2b7f16d5e32b5fac3
green-LED <greenodd@live.com>
2015-02-11 20:30:49 -0800

    fixed part1 not building

---
+++ project1/part2/Calculator/CalculatorViewController.h
@@ -0,0 +1,21 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by CS193p Instructor.
+//  Copyright (c) 2011 Stanford University.
+//  All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CalculatorBrain.h"
+
+
+
+@interface CalculatorViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *display;
+
+//TODO: add another property (of type outlet) that displays the o/p from descriptionOfProgram
+
+@end
---
+++ project1/part2/Calculator/GraphViewController.h
@@ -0,0 +1,17 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by CS193p Instructor.
+//  Copyright (c) 2011 Stanford University.
+//  All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface GraphViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (nonatomic, strong) id program;
+
+- (void)refreshView;
+
+@end
