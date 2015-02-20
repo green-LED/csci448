7b211cb4a31be95517a5eca7823de3595a5b996b
green-LED <greenodd@live.com>
2015-02-19 21:58:27 -0800

    added rest of sample code

---
+++ project1/part2/Calculator/GraphViewController.h
@@ -0,0 +1,18 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by CS193p Instructor.
+//  Copyright (c) 2011 Stanford University.
+//  All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface GraphViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (nonatomic, strong) id program;
+@property (weak, nonatomic) IBOutlet UILabel *description;
+
+- (void)refreshView;
+
+@end
